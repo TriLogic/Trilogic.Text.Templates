2d6b636c22d505aec8fe581f46d2d92321a19340
Andrew Friedl <affriedl@gmail.com>
2021-09-15 17:17:59 -0500

    Add project files.

---
+++ Trilogic.Text.TagReplacer/TagReplacer.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trilogic.Text.TagReplacer
+{
+    #region Provider Delegate
+    public delegate bool TagProvider(string Key, out string Value);
+    #endregion
+
+    public class TagReplacer
+    {
+        #region Class members
+        private ITagPattern mPattern = new StringTagPattern();
+        private List<TagToken> mTokens = new List<TagToken>();
+        private Stack<TagToken> mStack = new Stack<TagToken>();
+        #endregion
+
+        #region Constructors & Destructors
+        public TagReplacer()
+        {
+        }
+        public TagReplacer(string pattern)
+        {
+            Compile(pattern);
+        }
+        public TagReplacer(StringBuilder pattern)
+        {
+            Compile(pattern);
+        }
+        public TagReplacer(char[] pattern)
+        {
+            Compile(pattern);
+        }
+        public TagReplacer(ITagPattern pattern)
+        {
+            Compile(pattern);
+        }
+        #endregion
+
+        #region Compile Methods
+        public void Compile(string pattern)
+        {
+            Compile(new StringTagPattern(pattern));
+        }
+        public void Compile(StringBuilder pattern)
+        {
+            Compile(new StringBuilderTagPattern(pattern));
+        }
+        public void Compile(char[] pattern)
+        {
+            Compile(new CharArrayTagPattern(pattern));
+        }
+        public void Compile(ITagPattern pattern)
+        {
+            // store the pattern
+            mPattern = pattern == null ? new StringTagPattern() : pattern;
+
+            // generate a list of tokens
+            mTokens = TagTokenizer.Tokenize(mPattern);
+        }
+        #endregion
+
+        #region Replace with New Pattern
+        public void Replace(string pattern, TagProvider provider, StringBuilder target)
+        {
+            Compile(pattern);
+            Replace(provider, target);
+        }
+        public void Replace(StringBuilder pattern, TagProvider provider, StringBuilder target)
+        {
+            Compile(pattern);
+            Replace(provider, target);
+        }
+        public void Replace(char[] pattern, TagProvider provider, StringBuilder target)
+        {
+            Compile(pattern);
+            Replace(provider, target);
+        }
+        public void Replace(ITagPattern pattern, TagProvider provider, StringBuilder target)
+        {
+            Compile(pattern);
+            Replace(provider, target);
+        }
+        #endregion
+
+        #region Replace with Existing Pattern
+        public void Replace(TagProvider provider, StringBuilder target)
+        {
+            // assign an empty defaul provider
+            if (provider == null)
+            {
+                provider = delegate (string key, out string value) {
+                    value = $"[{key}]"; return true;
+                };
+            }
+
+            // clear the target
+            target.Clear();
+
+            // clear the stack
+            mStack.Clear();
+
+            // Process the tokens
+            foreach (TagToken tkn in mTokens)
+            {
+                // Text Token
+                if (tkn.IsTXT)
+                {
+                    // Append the text into the target StringBuilder
+                    target.Append(mPattern.Substring(tkn.TknOffset, tkn.TknLength));
+
+                    if (mStack.Count == 0)
+                    {
+                        mStack.Push(tkn.Clone(0));
+                    }
+                    else
+                    {
+                        // Include the new text to the TOS tag
+                        // and we don't care what type it is.
+                        mStack.Peek().TknLength += tkn.TknLength;
+                    }
+                    continue;
+                }
+
+                // Left hand tag
+                if (tkn.IsLHT)
+                {
+                    mStack.Push(tkn.Clone(target.Length, 0));
+                    continue;
+                }
+
+                // Right hand token
+                if (tkn.IsRHT)
+                {
+                    // If TOS is not a RHT then we have a format error
+                    if (mStack.Count < 1 || !mStack.Peek().IsLHT)
+                        throw new Exception($"Invalid pattern: mismtach at offset {tkn.TknOffset}");
+
+                    // We must have non-zero text for the Tag lookup
+                    if (mStack.Peek().TknLength == 0)
+                        throw new Exception($"Invalid pattern: empty key at offset {tkn.TknOffset}");
+
+                    TagToken keyToken = mStack.Peek();
+                    string keyValue = target.ToString(keyToken.TknOffset, keyToken.TknLength);
+
+                    // Are we trying to lookup an empty key?
+                    if (string.IsNullOrEmpty(keyValue))
+                        throw new Exception($"Invalid pattern: empty null of empty key at offset {tkn.TknOffset}");
+
+                    // the is the resulting text value
+                    string txtValue = null;
+
+                    if (!provider(keyValue, out txtValue))
+                        throw new Exception($"Invalid pattern: unresolved key [{keyValue}]");
+
+                    // remove the key text from the target
+                    target.Remove(keyToken.TknOffset, keyToken.TknLength);
+
+                    // Append the replacement text target (don't allow null)
+                    target.Append(string.IsNullOrEmpty(txtValue) ? string.Empty : txtValue);
+
+                    // Discard the key token
+                    if (mStack.Count == 1)
+                    {
+                        // First token was a LHT so it becomes a TXT an the only
+                        // text in the target is the result of the replacement.
+                        mStack.Peek().SetTXT(0, txtValue.Length);
+                        continue;
+                    }
+
+                    // Discard the TOS LHT
+                    mStack.Pop();
+
+                    // add the replacement text to the TOS token
+                    mStack.Peek().TknLength += txtValue.Length;
+                }
+            }
+
+            // The stack could contain only single item
+            if (mStack.Count != 1)
+                throw new Exception($"Invalid pattern: mismatch");
+
+        }
+        #endregion
+    }
+}
---
+++ Trilogic.Text.TagReplacer/TagTokenizer.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trilogic.Text.TagReplacer
+{
+    internal class TagTokenizer
+    {
+        #region Class Members
+        ITagPattern mBuf;
+
+        int mIdx = 0;
+        #endregion
+
+        #region Constructors and Destructors
+        public TagTokenizer(ITagPattern buf)
+        {
+            Reset(buf);
+        }
+        #endregion
+
+        #region Reset
+        public void Reset(ITagPattern buf)
+        {
+            mBuf = buf;
+            mIdx = 0;
+        }
+        #endregion
+
+        #region Token Retreival
+        public TagToken GetToken()
+        {
+            int offst = mIdx;
+            int chars = 0;
+
+            while (mIdx < mBuf.Length)
+            {
+                char tkc = mBuf[mIdx];
+
+                // Closure
+                if (tkc == '}')
+                {
+                    if (chars > 0)
+                        return new TagToken(TokenType.TkTXT, offst, chars);
+
+                    mIdx++;
+                    return new TagToken(TokenType.TkRHT, mIdx, 1);
+                }
+
+                // LHT or Escape
+                if (tkc == '$')
+                {
+                    // Is there room for a LHT or Escape?
+                    if (mIdx + 1 < mBuf.Length)
+                    {
+                        // Is this an escape - $$ or $}?
+                        if (mBuf[mIdx + 1] == '$' || mBuf[mIdx + 1] == '}')
+                        {
+                            if (chars > 0)
+                                return new TagToken(TokenType.TkTXT, offst, chars);
+
+                            mIdx += 2;
+                            return new TagToken(TokenType.TkTXT, offst + 1, 1);
+                        }
+
+                        // Is this a LHT - ${
+                        if (mBuf[mIdx + 1] == '{')
+                        {
+                            if (chars > 0)
+                                return new TagToken(TokenType.TkTXT, offst, chars);
+
+                            mIdx += 2;
+                            return new TagToken(TokenType.TkLHT, offst, 2);
+                        }
+
+                        // Dangling '$' char fall though
+                    }
+                }
+
+                // add to the growing list of characters
+                chars++;
+                mIdx++;
+            }
+
+            // if we have a token value
+            if (chars > 0)
+                return new TagToken(TokenType.TkTXT, offst, chars);
+
+            // No more tokens
+            return null;
+        }
+
+        public List<TagToken> GetTokens()
+        {
+            List<TagToken> list = new List<TagToken>();
+            TagToken tkn = GetToken();
+            while ((tkn = GetToken() ) != null)
+                list.Add(tkn);
+            return list;
+        }
+        public List<TagToken> GetTokens(ITagPattern pattern)
+        {
+            Reset(pattern);
+            return GetTokens();
+        }
+        public List<TagToken> GetTokens(string pattern)
+        {
+            Reset(new StringTagPattern(pattern));
+            return GetTokens();
+        }
+        public List<TagToken> GetTokens(StringBuilder pattern)
+        {
+            Reset(new StringBuilderTagPattern(pattern));
+            return GetTokens();
+        }
+        public List<TagToken> GetTokens(char[] pattern)
+        {
+            Reset(new CharArrayTagPattern(pattern));
+            return GetTokens();
+        }
+
+        public static List<TagToken> Tokenize(ITagPattern pattern)
+        {
+            return new TagTokenizer(pattern).GetTokens();
+        }
+        public static List<TagToken> Tokenize(string pattern)
+        {
+            return new TagTokenizer(new StringTagPattern(pattern)).GetTokens();
+        }
+        public static List<TagToken> Tokenize(StringBuilder pattern)
+        {
+            return new TagTokenizer(new StringBuilderTagPattern(pattern)).GetTokens();
+        }
+        public static List<TagToken> Tokenize(char[] pattern)
+        {
+            return new TagTokenizer(new CharArrayTagPattern(pattern)).GetTokens();
+        }
+
+        #endregion
+    }
+}
